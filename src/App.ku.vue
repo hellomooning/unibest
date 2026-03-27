55bbd62deac1c3a5b018dfe5528f7ddcc9683148
hellomooning <46080421+hellomooning@users.noreply.github.com>
2026-03-27 11:38:17 +0800

    Refactor tabbar visibility handling in App.ku.vue

--- src/App.ku.vue
+++ src/App.ku.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import FgTabbar from '@/tabbar/index.vue'
-import { isPageTabbar } from './tabbar/store'
+import { isPageTabbar, tabbarStore } from './tabbar/store'
 import { currRoute } from './utils'
 
-const isCurrentPageTabbar = ref(true)
 onShow(() => {
   console.log('App.ku.vue onShow', currRoute())
   const { path } = currRoute()
   // “蜡笔小开心”提到本地是 '/pages/index/index'，线上是 '/' 导致线上 tabbar 不见了
   // 所以这里需要判断一下，如果是 '/' 就当做首页，也要显示 tabbar
   if (path === '/') {
-    isCurrentPageTabbar.value = true
+    tabbarStore.isCurrentPageTabbar = true
   }
   else {
-    isCurrentPageTabbar.value = isPageTabbar(path)
+    tabbarStore.isCurrentPageTabbar = isPageTabbar(path)
   }
 })
 
@@ -36,6 +35,6 @@ defineExpose({
 
     <KuRootView />
 
-    <FgTabbar v-if="isCurrentPageTabbar" />
+    <FgTabbar v-if="tabbarStore.isCurrentPageTabbar" />
   </view>
 </template>
